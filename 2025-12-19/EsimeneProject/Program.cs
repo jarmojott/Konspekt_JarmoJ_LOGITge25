c297e20c368bfcc249df827c12aa33147e8787bb
Jarmo <jarmo.jogeva@gmail.com>
2026-01-07 23:56:53 +0000

    1 ⭐ näidisülesanne 3 (19.12.2025 antud)

--- 2025-12-19/EsimeneProject/Program.cs
+++ 2025-12-19/EsimeneProject/Program.cs
@@ -57,6 +57,20 @@ internal class Program
         - logib sisse kui kui parool ja nimi on õige
         Vooskeem juba tehtud
          */
+
+        Console.Write("Sisesta oma kasutajanimi: ");
+        String kasutajaNimi= Console.ReadLine();
+
+        Console.Write("Sisesta oma parool: ");
+        String kasutajaParool= Console.ReadLine();
+
+        if (kasutajaNimi=="jarmoj" && kasutajaParool=="jarmoParool")
+        {
+            Console.WriteLine("Oled edukalt sisse logitud!");
+        } else
+        {
+            System.Console.WriteLine("Kasutaja nimi või parool on vale!");
+        }
     }
 }
 
