8be8b89aaf4e29ec96d7f5d1457629a7b0000a1c
Jarmo <jarmo.jogeva@gmail.com>
2026-01-08 02:25:02 +0200

    Ülesannete kaust

---
+++ #Ülesanded/LOGITge25_Jarmo_Jõgeva_1-3.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        klient tahab programmi:
+        - küsib kasutaja nime
+        - küsib kasutaja parooli
+        - logib sisse kui kui parool ja nimi on õige
+        Vooskeem juba tehtud
+         */
+
+        Console.Write("Sisesta oma kasutajanimi: ");
+        String kasutajaNimi = Console.ReadLine();
+
+        Console.Write("Sisesta oma parool: ");
+        String kasutajaParool = Console.ReadLine();
+
+        if (kasutajaNimi == "jarmoj" && kasutajaParool == "jarmoParool")
+        {
+            Console.WriteLine("Oled edukalt sisse logitud!");
+        }
+        else
+        {
+            System.Console.WriteLine("Kasutaja nimi või parool on vale!");
+        }
+    }
+}
