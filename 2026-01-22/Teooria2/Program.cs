a1cacf994586a73a5d3b674148befe8fea89d34e
Opilane TTHK <opilane@tthk.ee>
2026-01-22 16:30:30 +0200

    switch teooria

---
+++ 2026-01-22/Teooria2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Teooria2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* <-- TINGIMUSLAUSE "SWITCHCASE" -->
+             * 'switch' on kaitstud sõna alternatiivse tingimuskontrolli jaoks, mida saab tüüpilise 'IF ELSE' asemel kasutada.
+             * Sulgude vahele jääb muutuja nimi või avaldis mille põhjal tingimuslik ümberlülitus toimub.
+             *  Sellele järgneb koodiblokk {} mille siis käivad juhtumid või väärtused mis võivad ühtida lülitusvalikuga.
+             *  Iga juhtum algab sõnaga 'case' ning väärtus selle järele millele see juhtub vastab. Kooloni järgi lisatakse tegevus mis tingimuse täitumisel täidetakse.
+             *  'default:' juhtub käivitub siis kui mitte üksi juhtumi väärtus ei vastanud tingimusele ehk siis vaikejuhtum. Erinvalt else lausest võib see olla ka bloki alguses.
+             *  'break' sõna määrab nii juhtumi tegevuse lõpu ning katkestab kogu switch koodibloki.
+             */
+            int option = 3;
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    Console.WriteLine(option);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
