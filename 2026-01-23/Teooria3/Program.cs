542550b9f142302cd13a729bbe0a49fad76d5d4f
Opilane TTHK <opilane@tthk.ee>
2026-01-23 09:42:41 +0200

    Parsimine ja Konverteerimine + näide pere pikkustega

---
+++ 2026-01-23/Teooria3/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Teooria3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* <-- PARSIMINE ehk teisendamine -->
+             * On katse teisendada tekstist mingeid arvandmeid.
+             * Teisendamine toimub küsides mingist andmetüübist talle sisseehitatud funktsiooni "Parse" välja kutsumisel mille parameetriks on sõne. 
+             * Eksisteerib ka alternatiivne meetod tryParse() mis suudab teisendamise ebaõnnestumisel tagastada booleani.
+             */
+            string seeOnTekst = "1";
+            int teisendatud = int.Parse(seeOnTekst);
+            //Console.WriteLine(teisendatud);
+            //Console.WriteLine(int.TryParse(seeOnTekst, out teisendatud));
+
+            /*  <-- KONVERTEERIMINE -->
+             *  Konverteerimine on otsene teisendus ükskõik millisesse teise andmetüüpi.
+             *  Selle jaoks eksisteerib eraldi moodul Convert kus on olemas erinevad meetodid kõikide andmetüüpide jaoks, sh. toString().
+             *  Sisendparameetrina aktseerib ta suvalist andmetüüpi.
+             * */
+            //var mingiInfo = "6.7";
+            //string mingiInfoTekst = Convert.ToString(mingiInfo);
+            //double mingiInfoDouble = Convert.ToDouble(mingiInfoTekst);
+            //char mingoInfoChart = Convert.ToChar(mingiInfoDouble);
+            //int mingiInfoInt = Convert.ToInt32(mingoInfoChart);
+            //long mingiInfoLong = Convert.ToInt64(mingoInfoChart);
+            //decimal mingiInfoDecimal = Convert.ToDecimal(mingiInfoLong);
+            //byte mingiInfoByte = Convert.ToByte(mingiInfoDecimal);
+            //bool mingiInfoBool = Convert.ToBoolean(mingiInfoByte);
+
+            //NÄIDE:
+
+            int kasutajaPikkus = 0;
+            int vennaPikkus = 0;
+            int emaPikkus = 0;
+            int isaPikkus = 0;
+
+            Console.Write("Mis on sinu pikkus (cm): ");
+            kasutajaPikkus = int.Parse(Console.ReadLine());
+
+            Console.Write("Mis on sinu venna pikkus (cm): ");
+            vennaPikkus = int.Parse(Console.ReadLine());
+
+            Console.Write("Mis on sinu ema pikkus (cm): ");
+            emaPikkus = int.Parse(Console.ReadLine());
+
+            Console.Write("Mis on sinu isa pikkus (cm): ");
+            isaPikkus = int.Parse(Console.ReadLine());
+
+            // manuaalne castimine
+            float pereKeskmine = ((float)kasutajaPikkus + vennaPikkus + emaPikkus + isaPikkus) / 4 / 100; //meetrites
+            float vanemateKeskmine = ((float)emaPikkus + isaPikkus) / 2 / 100; //meetrites
+            float lasteKeskmine = ((float)kasutajaPikkus + vennaPikkus) / 2 / 100; //meetrites
+
+            float vahe = 0;
+
+            if (vanemateKeskmine > lasteKeskmine)
+                vahe = vanemateKeskmine - lasteKeskmine;
+            else
+                vahe = lasteKeskmine - vanemateKeskmine;
+
+            Console.WriteLine("Perekeskmine on " + Math.Round(pereKeskmine, 2) + "m.\nVanematekeskmine on " + Math.Round(vanemateKeskmine, 2) + "m.\nLastekeskmine on " + Math.Round(lasteKeskmine, 2) + "m.\nVahe on " + Math.Round(vahe, 2) + "m.");
+        }
+    }
+}
