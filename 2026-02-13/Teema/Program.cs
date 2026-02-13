0665ab53a6d4f91d28be54dfa2fc71be154f94ea
Opilane TTHK <opilane@tthk.ee>
2026-02-13 13:37:24 +0200

    Dictionary eks sõnastik

--- 2026-02-13/Teema/Program.cs
+++ 2026-02-13/Teema/Program.cs
@@ -23,6 +23,40 @@
         double kaheKomaga = juhuObjekt.NextDouble();
         float üheKomaga  = juhuObjekt.NextSingle();
 
+
+        /*
+         * ### KOMPOSIITANDMETÜÜBID ###
+         * 1. Massiiv
+         * 2. Loend
+         * 2. Sõnastik
+         * 3. Tuple
+         * 
+         * <-- SÕNASTIK -->
+         * Dictionary on komposiitandmetüüp mis omab võti/väärtus paare.
+         * Erinevalt teistest komposiitandmetüüpidest saab Dictionary omada 2 erinevat või sama andmetüüpi väärtust mis sümboliseerivadki võtit ning tema väärtust.
+         * Samuti võib andmetüübiks olla ka klass.
+         * Andmetüüp Dictionary näitab ära sõnastiku klassi, selle järel noolsulgude sees on 2 andmetüüpi: esimene on võti, ja teine tema väärtus.
+         * new käsusõna käivitab konstruktori mis loob uue tühja sõnastiku vastavate andmetüüpidega noolsulgude vahel.
+         * Võimalik on uus sõnastik ka kohe algväärtustada andmetega kasutades topelt loogelisi sulge {{}}
+         * 
+         * <-- SÕNASTIKU TÖÖRIISTAD -->
+         * Meetod add() võimaldab lisada sõnastikku lõppu uue paari andes kaasa parameetridena sulgude sees võtmepaari väärtused.
+         * Meetod Remove() võimaldab eemalda ühe võtmepaari kasutades parameetrina võtme väärtust.
+         * Meetod Clear() eemaldab kõik võtmepaarid sõnastikust.
+         * Meetod ContainsKey() kontrollib kas sõnastikus eksisteerib vastav võti ning tagastab meile tulemuse bool'ina.
+         * Meetod ContainsValue() kontrollib kas sõnastikus eksisteerib vastav väärtus ning tagastab meile tulemuse bool'ina.
+         */
+        Dictionary<int, string> sõnastik1 = new Dictionary<int, string>();
+        Dictionary<int, string> sõnastik2 = new Dictionary<int, string>() { {1, "essa" }, {2, "tessa" }, { 3, "kossa" } };
+        ;
+        Dictionary<List<int>, string> sõnastik3 = new Dictionary<List<int>, string>();
+        sõnastik2.Add(4, "tikker");
+        sõnastik2.Remove(4);
+        sõnastik2.Clear();
+        sõnastik1.ContainsKey(4);
+        sõnastik1.ContainsValue("uus");
+
+
     }
 }
 
